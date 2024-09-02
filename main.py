0fc64e74cab0afddbc76c228dc7c850fc3a86205
FJDubs <taggartf1@gmail.com>
2024-09-02 19:06:30 -0400

    gets word count and character counts from text

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
-    print(text)
+    word_count = get_word_count(text)
+    char_counts = char_count(text)
+    print(char_counts)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def char_count(text):
+    char_counts = {}
+    for t in text.lower():
+        if t not in char_counts.keys():
+            char_counts[t] = 0
+        char_counts[t] += 1
+    return char_counts
 
 main()
