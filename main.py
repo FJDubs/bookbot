1bf239eb7489f942d50846c9aa83e607cf817725
FJDubs <taggartf1@gmail.com>
2024-09-02 18:48:39 -0400

    open and read text file books/frankenstein.txt

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    path_to_file = "books/frankenstein.txt"
+    text = get_book_text(path_to_file)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
